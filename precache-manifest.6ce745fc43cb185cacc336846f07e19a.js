a6d0c4a837c6f28ba6123135e0f3c4ea800022ce
seanren96 <seanren96@gmail.com>
2019-11-13 19:15:36 +1100

    Updates

---
+++ precache-manifest.6ce745fc43cb185cacc336846f07e19a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40a5983d5a17a78592883ecd63fffa9",
+    "url": "/Logger/index.html"
+  },
+  {
+    "revision": "42cfb2d037ffdca3e4ff",
+    "url": "/Logger/static/css/main.dd5ea27c.chunk.css"
+  },
+  {
+    "revision": "614dfed0f28ac38811e3",
+    "url": "/Logger/static/js/2.d7274656.chunk.js"
+  },
+  {
+    "revision": "42cfb2d037ffdca3e4ff",
+    "url": "/Logger/static/js/main.93078e6c.chunk.js"
+  },
+  {
+    "revision": "547a1998bf3dccffbff1",
+    "url": "/Logger/static/js/runtime-main.73d35afa.js"
+  }
+]);
